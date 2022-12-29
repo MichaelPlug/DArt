467cd93e0c320def4ca29a0dd31554acf289d139
MichaelPlug <michelespina96@gmail.com>
2022-12-29 12:26:59 +0100

    initialize Patron smartcontract

--- Patron.sol
+++ Patron.sol
@@ -1,7 +1,29 @@
 //for compability reasons, we work only with these versions
 pragma solidity >=0.7.0 < 0.9.0;
 
+import "DCoin.sol";
+
 contract Patron {
 
-    // potremmo dare anche due input al dizionario come key
+    mapping (bytes32 => uint) public patronCredit;
+    mapping (bytes32 => uint) public funds;
+
+
+    function crowfunding(bytes32 artwork, uint amount) external {
+
+    }
+
+    function viewFunds(bytes32 artwork) external view returns(uint){
+        return funds[artwork];
+    }
+
+    function viewPatronCredit(address patron, address museum) external view returns(uint){
+        return patronCredit[keccak256(abi.encodePacked(patron,museum))];
+    }
+
+    function moveFunds(address _to) internal {
+
+    }
+
+    function burn(uint amount){}
 }
